e20004382008ac0e4a6def5d419f85869e2ccb2b
gpigp <rlaxogus505@naver.com>
2021-04-26 22:02:22 +0900

    error detect

--- app/src/main/java/android/cs/pusan/ac/myapplication/Interesting_Cat.java
+++ app/src/main/java/android/cs/pusan/ac/myapplication/Interesting_Cat.java
@@ -9,6 +9,6 @@ public class Interesting_Cat extends AppCompatActivity {
     @Override
     protected void onCreate(@Nullable Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);
+        setContentView(R.layout.pick_cat);
     }
 }
