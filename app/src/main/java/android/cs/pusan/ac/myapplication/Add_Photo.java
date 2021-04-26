976cf85c97123d8c8a31b35222da4848aa5ed293
gpigp <rlaxogus505@naver.com>
2021-04-26 21:46:40 +0900

    added gallery button

---
+++ app/src/main/java/android/cs/pusan/ac/myapplication/Add_Photo.java
@@ -0,0 +1,36 @@
+package android.cs.pusan.ac.myapplication;
+
+import android.content.Intent;
+import android.net.Uri;
+import android.os.Bundle;
+import android.widget.ImageView;
+
+import androidx.annotation.Nullable;
+import androidx.appcompat.app.AppCompatActivity;
+
+public class Add_Photo extends AppCompatActivity {
+
+    private final int GET_GALLERY_IMAGE = 200;
+    private ImageView imageview;
+
+    @Override
+    protected void onCreate(@Nullable Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.take_gallery);
+
+        imageview = (ImageView)findViewById(R.id.imageView);
+
+        Intent gallery = new Intent(Intent.ACTION_PICK);
+        gallery.setDataAndType(android.provider.MediaStore.Images.Media.EXTERNAL_CONTENT_URI, "image/*");
+        startActivityForResult(gallery, GET_GALLERY_IMAGE);
+    }
+
+    @Override
+    protected void onActivityResult(int requestCode, int resultCode, Intent data) {
+        super.onActivityResult(requestCode, resultCode, data);
+        if (requestCode == GET_GALLERY_IMAGE && resultCode == RESULT_OK && data != null && data.getData() != null) {
+            Uri selectedImageUri = data.getData();
+            imageview.setImageURI(selectedImageUri);
+        }
+    }
+}
