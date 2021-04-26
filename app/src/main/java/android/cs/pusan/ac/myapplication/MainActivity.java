5bb75230cc54bf16bd866fc8c5419616af3d1e1c
gpigp <rlaxogus505@naver.com>
2021-04-26 20:31:37 +0900

    aaa

--- app/src/main/java/android/cs/pusan/ac/myapplication/MainActivity.java
+++ app/src/main/java/android/cs/pusan/ac/myapplication/MainActivity.java
@@ -40,7 +40,7 @@ public class MainActivity extends AppCompatActivity
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-
+//
         SupportMapFragment mapFragment = (SupportMapFragment) getSupportFragmentManager()
                 .findFragmentById(R.id.map);
         mapFragment.getMapAsync(this);
