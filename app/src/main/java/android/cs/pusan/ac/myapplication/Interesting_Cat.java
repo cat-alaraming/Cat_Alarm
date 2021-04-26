a84c012189bb5cce1266ce0240511557a705f21b
gpigp <rlaxogus505@naver.com>
2021-04-26 22:00:26 +0900

    added gallery button

---
+++ app/src/main/java/android/cs/pusan/ac/myapplication/Interesting_Cat.java
@@ -0,0 +1,14 @@
+package android.cs.pusan.ac.myapplication;
+
+import android.os.Bundle;
+
+import androidx.annotation.Nullable;
+import androidx.appcompat.app.AppCompatActivity;
+
+public class Interesting_Cat extends AppCompatActivity {
+    @Override
+    protected void onCreate(@Nullable Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
