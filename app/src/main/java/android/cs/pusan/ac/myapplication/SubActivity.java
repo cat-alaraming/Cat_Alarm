887a90343ef64c757e447d209796704924619d21
김태현 <rlaxogus505@naver.com>
2021-04-26 11:59:04 +0900

    aaa

---
+++ app/src/main/java/android/cs/pusan/ac/myapplication/SubActivity.java
@@ -0,0 +1,9 @@
+package android.cs.pusan.ac.myapplication;
+
+public class SubActivity extends AppCompatActivity {
+    @Override
+    protected void onCreate(@Nullable Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.information);
+    }
+}
