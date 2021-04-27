499ee9c3fe2f839aaca9cbb96c9919fc85038555
eyi-jin <jmj2588547@naver.com>
2021-04-28 02:30:14 +0900

    회원가입 일단 코드 옮김

--- app/src/main/java/android/cs/pusan/ac/myapplication/RegisterActivity.java
+++ app/src/main/java/android/cs/pusan/ac/myapplication/RegisterActivity.java
@@ -1,14 +1,87 @@
 package android.cs.pusan.ac.myapplication;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.app.ProgressDialog;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.Toast;
+
+import com.google.android.gms.tasks.OnCompleteListener;
+import com.google.android.gms.tasks.Task;
+import com.google.firebase.auth.AuthResult;
+import com.google.firebase.auth.FirebaseAuth;
 
 public class RegisterActivity extends AppCompatActivity {
+
+    private EditText editEmail;
+    private EditText editPassword;
+    private Button registerBtn;
+
+    private FirebaseAuth firebaseAuth;
+    ProgressDialog progressDialog;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_register);
+
+
+        progressDialog = new ProgressDialog(this);
+        progressDialog.setTitle("Loading");
+        progressDialog.setMessage("Please wait...");
+
+        editEmail = findViewById(R.id.text_register_id);
+        editPassword = findViewById(R.id.text_register_password);
+        registerBtn = findViewById(R.id.real_register_button);
+
+        firebaseAuth = FirebaseAuth.getInstance();
+
+        registerBtn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                progressDialog.show();
+                String email = editEmail.getText().toString();
+                String password = editPassword.getText().toString();
+
+                createAccount(email, password);
+            }
+        });
+    }
+
+
+    private void createAccount(String email, String password) {
+        // [START create_user_with_email]
+        firebaseAuth.createUserWithEmailAndPassword(email, password)
+                .addOnCompleteListener(this, new OnCompleteListener<AuthResult>() {
+                    @Override
+                    public void onComplete(@NonNull Task<AuthResult> task) {
+                        progressDialog.hide();
+                        if (task.isSuccessful()) {
+
+                            firebaseAuth.getCurrentUser().sendEmailVerification().addOnCompleteListener(new OnCompleteListener<Void>() {
+                                @Override
+                                public void onComplete(@NonNull Task<Void> task) {
+                                    if(task.isSuccessful()){
+                                        Toast.makeText(RegisterActivity.this, "User registered successfully. Please verify your email id",
+                                                Toast.LENGTH_SHORT).show();
+                                    }else{
+                                        // If sign in fails, display a message to the user.\
+                                        Toast.makeText(RegisterActivity.this, "Failed to register",
+                                                Toast.LENGTH_SHORT).show();
+                                    }
+                                }
+                            });
+                        } else {
+                            // If sign in fails, display a message to the user.\
+                            Toast.makeText(RegisterActivity.this, "Failed to register",
+                                    Toast.LENGTH_SHORT).show();
+                        }
+                    }
+                });
+        // [END create_user_with_email]
     }
 }
