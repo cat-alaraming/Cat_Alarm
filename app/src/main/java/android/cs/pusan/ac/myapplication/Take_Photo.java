bc203e7904c899823b78b439cc4119e577b3e11a
gpigp <rlaxogus505@naver.com>
2021-05-10 23:34:56 +0900

    카메라 수정

--- app/src/main/java/android/cs/pusan/ac/myapplication/Take_Photo.java
+++ app/src/main/java/android/cs/pusan/ac/myapplication/Take_Photo.java
@@ -44,6 +44,9 @@ public class Take_Photo extends AppCompatActivity {
 
     Spinner spinner;
     String selected;
+    Spinner spinner2;
+    String selected2;
+    String allNames = "";
 
     private FirebaseFirestore mDatabase;
     private ArrayList<Uri> mArrayUri;
@@ -68,6 +71,7 @@ public class Take_Photo extends AppCompatActivity {
             public void onItemSelected(AdapterView<?> parent, View view, int position, long id) {
                 selected = catNames.get(position);
             }
+
             @Override
             public void onNothingSelected(AdapterView<?> parent) {
                 selected = catNames.get(0);
@@ -75,23 +79,109 @@ public class Take_Photo extends AppCompatActivity {
         });
 
         Button btn_uploadImages = findViewById(R.id.btn_uploadImages);
-        btn_uploadImages.setOnClickListener((v-> {
+        btn_uploadImages.setOnClickListener((v -> {
             Intent takePictureIntent = new Intent(MediaStore.ACTION_IMAGE_CAPTURE);
             if (takePictureIntent.resolveActivity(getPackageManager()) != null) {
                 File photoFile = null;
-                try{
+                try {
                     photoFile = createImageFile();
-                }catch(IOException ex) {
+                } catch (IOException ex) {
                     onBackPressed();
                 }
-                if(photoFile != null){
-                    photoUri = FileProvider.getUriForFile(getApplicationContext(),getPackageName(), photoFile);
+                if (photoFile != null) {
+                    photoUri = FileProvider.getUriForFile(getApplicationContext(), getPackageName(), photoFile);
                     takePictureIntent.putExtra(MediaStore.EXTRA_OUTPUT, photoUri);
                     startActivityForResult(takePictureIntent, REQUEST_IMAGE_CAPTURE);
                 }
             }
         }));
+
+        EditText editText_name = findViewById(R.id.editText_name);
+        EditText editText_features = findViewById(R.id.editText_features);
+        spinner2 = findViewById(R.id.spinner2);
+        String[] types = {"black1", "black2", "cheese", "godeung", "chaos", "samsaek"};
+        ArrayAdapter<String> adapter2 = new ArrayAdapter<>(this, android.R.layout.simple_spinner_item, types);
+        adapter2.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item);
+        spinner2.setAdapter(adapter2);
+        spinner2.setOnItemSelectedListener(new AdapterView.OnItemSelectedListener() {
+            @Override
+            public void onItemSelected(AdapterView<?> parent, View view, int position, long id) {
+                selected2 = types[position];
+            }
+
+            @Override
+            public void onNothingSelected(AdapterView<?> parent) {
+                selected2 = types[0];
+            }
+        });
+        Button btn_uploadNewCat = findViewById(R.id.btn_uploadNewCat);
+        btn_uploadNewCat.setOnClickListener(v -> {
+            String getCatName = editText_name.getText().toString();
+            String getFeature = editText_features.getText().toString();
+
+            Map<String, Object> data = new HashMap<>();
+            data.put("name", getCatName);
+            data.put("type", selected2);
+            int pm = 1;
+            int pm2 = 1;
+            if (Math.random() < 0.5) pm = -1;
+            if (Math.random() < 0.5) pm2 = -1;
+            data.put("latitude", 35.233 + pm * Math.random() * 0.005);
+            data.put("longitude", 129.08 + pm2 * Math.random() * 0.005);
+            mDatabase.collection("catMarkers")
+                    .add(data)
+                    .addOnSuccessListener(documentReference -> Log.d("ADD", "Document added ID: " + documentReference.getId()))
+                    .addOnFailureListener(e -> Log.d("ADD", "Error adding: ", e));
+
+            data = new HashMap<>();
+            data.put("names", getCatName);
+            data.put("features", getFeature);
+            data.put("num", 0);
+            mDatabase.collection("catInfo").document(getCatName)
+                    .set(data);
+            data = new HashMap<>();
+            data.put(getCatName, getCatName);
+            mDatabase.collection("catNamesNums").document("names")
+                    .set(data, SetOptions.merge());
+            data = new HashMap<>();
+            data.put(getCatName, 0);
+            mDatabase.collection("catNamesNums").document("nums")
+                    .set(data, SetOptions.merge());
+
+            mDatabase.document("catNamesNums/names")
+                    .get()
+                    .addOnCompleteListener(task -> {
+                        if (task.isSuccessful()) {
+                            Map<String, Object> getDB = task.getResult().getData();
+                            if (getDB == null) {
+                                Log.d("DB Error", "Error get DB no data", task.getException());
+                                return;
+                            }
+                            Object ob;
+                            if ((ob = getDB.get("allNames")) != null) {
+                                allNames = ob.toString() + "," + getCatName;
+                                Log.d("AllNames", "allnames " + allNames);
+                                mDatabase.document("catNamesNums/names").update("allNames", allNames);
+                            } else {
+                                Log.d("AllNames", "Error");
+                            }
+                        } else {
+                            Log.d("SHOW", "Error show DB", task.getException());
+                        }
+                    });
+
+            editText_name.setText(null);
+            editText_features.setText(null);
+
+        });
+        Button btn_goToAlbum = findViewById(R.id.btn_goToAlbum);
+        btn_goToAlbum.setOnClickListener(v -> {
+            Intent intent = new Intent(getApplicationContext(), showAlbum.class);
+            startActivity(intent);
+        });
     }
+
+
 
     private File createImageFile() throws IOException {
         String timeStamp = new SimpleDateFormat("yyyyMMdd_HHmmss").format(new Date());
