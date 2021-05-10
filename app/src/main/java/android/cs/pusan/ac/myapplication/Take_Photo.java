690141ccf1060d2ba96fbeb5334f5a7fb1e1ee88
gpigp <rlaxogus505@naver.com>
2021-05-10 20:17:07 +0900

    카메라 -> DB 타입설정안하면 팅겨요..

--- app/src/main/java/android/cs/pusan/ac/myapplication/Take_Photo.java
+++ app/src/main/java/android/cs/pusan/ac/myapplication/Take_Photo.java
@@ -52,35 +52,15 @@ public class Take_Photo extends AppCompatActivity {
     private ArrayList<Uri> mArrayUri;
     long num = 0;
     ArrayList<String> catNames;
-    String allNames = "";
 
 
     @Override
     protected void onCreate(@Nullable Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.information);
-        
-        Intent takePictureIntent = new Intent(MediaStore.ACTION_IMAGE_CAPTURE);
-        if (takePictureIntent.resolveActivity(getPackageManager()) != null) {
-            File photoFile = null;
-            try{
-                photoFile = createImageFile();
-            }catch(IOException ex) {
-                onBackPressed();
-            }
-            if(photoFile != null){
-                photoUri = FileProvider.getUriForFile(getApplicationContext(),getPackageName(), photoFile);
-                takePictureIntent.putExtra(MediaStore.EXTRA_OUTPUT, photoUri);
-                startActivityForResult(takePictureIntent, REQUEST_IMAGE_CAPTURE);
-            }
-        }
-
+        setContentView(R.layout.camera);
 
         mDatabase = FirebaseFirestore.getInstance();
-        Button btn_map = findViewById(R.id.btn_map);
-        btn_map.setOnClickListener(v -> {
-            onBackPressed();
-        });
+
         catNames = MainActivity.catNames;
 
         spinner = findViewById(R.id.spinner);
@@ -98,100 +78,26 @@ public class Take_Photo extends AppCompatActivity {
             }
         });
 
-        EditText editText_name = findViewById(R.id.editText_name);
-        EditText editText_features = findViewById(R.id.editText_features);
-        spinner2 = findViewById(R.id.spinner2);
-        String[] types = {"black1", "black2", "cheese", "godeung", "chaos", "samsaek"};
-        ArrayAdapter<String> adapter2 = new ArrayAdapter<>(this, android.R.layout.simple_spinner_item, types);
-        adapter2.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item);
-        spinner2.setAdapter(adapter2);
-        spinner2.setOnItemSelectedListener(new AdapterView.OnItemSelectedListener() {
-            @Override
-            public void onItemSelected(AdapterView<?> parent, View view, int position, long id) {
-                selected2 = types[position];
-            }
-            @Override
-            public void onNothingSelected(AdapterView<?> parent) {
-                selected2 = types[0];
-            }
-        });
-        Button btn_uploadNewCat = findViewById(R.id.btn_uploadNewCat);
-        btn_uploadNewCat.setOnClickListener(v -> {
-            String getCatName = editText_name.getText().toString();
-            String getFeature = editText_features.getText().toString();
-
-            Map<String, Object> data = new HashMap<>();
-            data.put("name", getCatName);
-            data.put("type", selected2);
-            int pm = 1; int pm2 = 1;
-            if( Math.random() < 0.5 ) pm = -1;
-            if( Math.random() < 0.5 ) pm2 = -1;
-            data.put("latitude", 35.233 + pm * Math.random()*0.005);
-            data.put("longitude", 129.08 + pm2 * Math.random()*0.005);
-            mDatabase.collection("catMarkers")
-                    .add(data)
-                    .addOnSuccessListener(documentReference -> Log.d("ADD","Document added ID: "+documentReference.getId()))
-                    .addOnFailureListener(e -> Log.d("ADD","Error adding: ",e));
-
-            data = new HashMap<>();
-            data.put("names", getCatName);
-            data.put("features", getFeature);
-            data.put("num", 0);
-            mDatabase.collection("catInfo").document(getCatName)
-                    .set(data);
-            data = new HashMap<>();
-            data.put(getCatName, getCatName);
-            mDatabase.collection("catNamesNums").document("names")
-                    .set(data, SetOptions.merge());
-            data = new HashMap<>();
-            data.put(getCatName, 0);
-            mDatabase.collection("catNamesNums").document("nums")
-                    .set(data, SetOptions.merge());
-
-            mDatabase.document("catNamesNums/names")
-                    .get()
-                    .addOnCompleteListener(task -> {
-                        if( task.isSuccessful() ){
-                            Map<String, Object> getDB = task.getResult().getData();
-                            if( getDB == null ){
-                                Log.d("DB Error", "Error get DB no data", task.getException());
-                                return;
-                            }
-                            Object ob;
-                            if( (ob = getDB.get("allNames")) != null ){
-                                allNames = ob.toString() + "," + getCatName;
-                                Log.d("AllNames", "allnames " + allNames);
-                                mDatabase.document("catNamesNums/names").update("allNames", allNames);
-                            }
-                            else{
-                                Log.d("AllNames", "Error");
-                            }
-                        }
-                        else{
-                            Log.d("SHOW", "Error show DB", task.getException());
-                        }
-                    });
-
-            editText_name.setText(null);
-            editText_features.setText(null);
-
-        });
-        Button btn_goToAlbum = findViewById(R.id.btn_goToAlbum);
-        btn_goToAlbum.setOnClickListener(v -> {
-            Intent intent = new Intent(getApplicationContext(), showAlbum.class);
-            startActivity(intent);
-        });
 
         Button btn_uploadImages = findViewById(R.id.btn_uploadImages);
+        btn_uploadImages.setOnClickListener((v-> {
+            Intent takePictureIntent = new Intent(MediaStore.ACTION_IMAGE_CAPTURE);
+            if (takePictureIntent.resolveActivity(getPackageManager()) != null) {
+                File photoFile = null;
+                try{
+                    photoFile = createImageFile();
+                }catch(IOException ex) {
+                    onBackPressed();
+                }
+                if(photoFile != null){
+                    photoUri = FileProvider.getUriForFile(getApplicationContext(),getPackageName(), photoFile);
+                    takePictureIntent.putExtra(MediaStore.EXTRA_OUTPUT, photoUri);
+                    startActivityForResult(takePictureIntent, REQUEST_IMAGE_CAPTURE);
+                }
+            }
+        }));
     }
 
-//    @Override
-//    protected void onActivityResult(int requestCode, int resultCode, Intent data) {
-//        super.onActivityResult(requestCode, resultCode, data);
-//        if (requestCode == REQUEST_IMAGE_CAPTURE && resultCode == RESULT_OK) {
-//            ((ImageView)findViewById(R.id.imageView)).setImageURI(photoUri);
-//        } else onBackPressed();
-//    }
 
     private File createImageFile() throws IOException {
         String timeStamp = new SimpleDateFormat("yyyyMMdd_HHmmss").format(new Date());
@@ -202,7 +108,6 @@ public class Take_Photo extends AppCompatActivity {
                 ".jpg",         /* suffix */
                 storageDir          /* directory */
         );
-        imageFilePath = image.getAbsolutePath();
         return image;
     }
 
@@ -213,32 +118,27 @@ public class Take_Photo extends AppCompatActivity {
         super.onActivityResult(requestCode, resultCode, data);
         mArrayUri = new ArrayList<>();
 
-        if (requestCode == 0 && resultCode == RESULT_OK && data != null) {
-//            여기서부터
-            ((ImageView)findViewById(R.id.imageView)).setImageURI(photoUri);
-            mArrayUri.add(photoUri);
-            uploadFile(selected);
-        }
-//        여기까지 지우면됨
+        if (requestCode == REQUEST_IMAGE_CAPTURE && resultCode == RESULT_OK) {
+
 
 //            // Get the Image from data
 //            if (data.getClipData() != null) {
 //                ClipData mClipData = data.getClipData();
 //                int cnt = mClipData.getItemCount();
-//                for (int i = 0; i < cnt; i++) {
-//                    Uri imageuri = mClipData.getItemAt(i).getUri();
-//                    mArrayUri.add(imageuri);
-//                }
+////                for (int i = 0; i < cnt; i++) {
+////                    Uri imageuri = mClipData.getItemAt(i).getUri();
+            mArrayUri.add(photoUri);
+////                }
 //            }
 //            else {
-//                Uri imageuri = data.getData();
-//                mArrayUri.add(imageuri);
+////                Uri imageuri = data.getData();
+//                mArrayUri.add(photoUri);
 //            }
-//            uploadFile(selected);
-//        }
-//        else{
-//            Toast.makeText(this, "사진 선택 취소", Toast.LENGTH_LONG).show();
-//        }
+            uploadFile(selected);
+        }
+        else{
+            Toast.makeText(this, "사진 선택 취소", Toast.LENGTH_LONG).show();
+        }
 
     }
 
@@ -272,8 +172,16 @@ public class Take_Photo extends AppCompatActivity {
                                             double progress = (100f * taskSnapshot.getBytesTransferred()) / taskSnapshot.getTotalByteCount();
                                         });
                             }
-                            mDatabase.document("catInfo/"+catName).update("num", num);
-                            mDatabase.document("catNamesNums/nums").update(catName, num);
+
+//                            if(data.getClipData() != null){
+                                mDatabase.document("catInfo/"+catName).update("num", num);
+                                mDatabase.document("catNamesNums/nums").update(catName, num);
+                                onBackPressed();
+//                            } else {
+//                                onBackPressed();
+//                                Toast.makeText(getApplicationContext(), "타입을 설정해주세요", Toast.LENGTH_SHORT).show();
+//                            }
+
                         }
                         else{
                             Log.d("SHOW", "Error show DB", task.getException());
